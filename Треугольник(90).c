8daf7412028c13102bb03b6e831a851293dd4c33
logvas <thelogvasstudio@gmail.com>
2018-04-19 02:32:46 +0700

    final triangle(90)
    Был  загружен неверный файл

---
+++ Треугольник(90).c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int i,j,n,k,m;
+scanf("%d%d",&n,&m);
+int A[n][m];//при массиве, содержащем не только фигуру, необходимо добавить шаг с обнулением массива
+
+for (i=0;i<n;i++)
+{
+for (j=0;j<m;j++)
+{
+A[i][j]=1;
+}
+}
+//углы
+for (i=2;i<n-2;i++)
+{
+A[i][m-1]=0;
+A[i][0]=0;
+}
+for (i=2;i<m-2;i++)
+{
+A[n-1][i]=0;
+A[0][i]=0;
+}
+
+A[1][1]=0;
+A[1][m-2]=0;
+A[n-2][1]=0;
+A[n-2][m-2]=0;
+
+ FILE*out;
+    out=fopen ("output.txt","w");
+for (j=0;j<m;j++)
+{
+for (i=0;i<n;i++)
+{
+fprintf(out,"%d ",A[i][j]);
+printf("%d ",A[i][j]);
+}
+fprintf(out,"\n");
+printf("\n");
+}
+fclose(out);
+printf("Write something to exit");
+scanf("%d",&n);
+}
